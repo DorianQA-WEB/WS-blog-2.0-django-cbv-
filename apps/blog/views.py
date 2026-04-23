d2f7bc5cb3ede0de2d829887c682e4f9b80cfb6e
DorianQA-WEB <qa.doriangrey@gmail.com>
2026-04-23 22:29:20 +0500

    pagination

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -7,10 +7,13 @@ class PostListView(ListView):
     model = Post
     template_name = 'blog/post_list.html'
     context_object_name = 'posts'
+    paginate_by = 2
 
     def get_content_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = 'Главная страница'
+        context['title'] = 'Главная страницы'
+        page = context['page_obj']
+        context['paginator_range'] = page.paginator.get_elided_page_range(page.number)
         return context
 
 
@@ -26,13 +29,11 @@ class PostDetailView(DetailView):
         return context
 
 
-
-
-
 class PostFromCategory(ListView):
     template_name = 'blog/post_list.html'
     context_object_name = 'posts'
     category = None
+    paginate_by = 1
 
     def get_queryset(self):
         self.category = Category.objects.get(slug=self.kwargs['slug'])
@@ -45,4 +46,6 @@ class PostFromCategory(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = f'Записи из категории: {self.category.title}'
+        page = context['page_obj']
+        context['paginator_range'] = page.paginator.get_elided_page_range(page.number)
         return context
