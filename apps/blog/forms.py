170ed78118bbca1b86eaadcb882cce9948180b86
DorianQA-WEB <qa.doriangrey@gmail.com>
2026-05-05 11:37:01 +0500

    csrf error

--- apps/blog/forms.py
+++ apps/blog/forms.py
@@ -48,7 +48,7 @@ class CommentCreateForm(forms.ModelForm):
     Форма добавления комментариев к статьям
     """
     parent = forms.IntegerField(widget=forms.HiddenInput, required=False)
-    content = forms.CharField(label='', wiget=forms.Textarea(
+    content = forms.CharField(label='', widget=forms.Textarea(
         attrs={'cols': 30, 'rows': 5, 'placeholder': 'Комментарий', 'class': 'form-control' }))
 
     class Meta:
